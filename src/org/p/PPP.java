41be3e848517bbbdb6048265e0832ba61cd47946
Morvine16 <prabasanpra@gmail.com>
2023-01-29 13:46:23 +0530

    first commit

---
+++ src/org/p/PPP.java
@@ -0,0 +1,17 @@
+package org.p;
+
+public class PPP {
+private void ONE() {
+System.out.println("this");
+}private void TWO() {
+System.out.println("two");
+}private void THREE() {
+System.out.println("there");
+}
+public static void main(String[] args) {
+	PPP p=new PPP();
+	p.ONE();
+	p.TWO();
+	p.THREE();
+}
+}
